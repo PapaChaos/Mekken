0fa3d307c42ac4aaa617eea6019a3b2e47d6c8c2
johnklima <klima.de.sa@gmail.com>
2019-01-16 12:29:11 +0100

    add texture animation

---
+++ Assets/UVamimation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UVamimation : MonoBehaviour
+{
+
+    public Mesh mesh;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2[] uvs = mesh.uv;
+
+        for (int i = 0; i < uvs.Length; i++)
+        {
+            uvs[i].x += Time.deltaTime * speed; // Mathf.Sin(Time.time) * Time.deltaTime * 0.1f;
+            //uvs[i].y += Mathf.Cos(Time.time) * Time.deltaTime * 0.1f;
+        }
+
+        //set uvs
+        mesh.uv = uvs;
+
+
+
+    }
+}
