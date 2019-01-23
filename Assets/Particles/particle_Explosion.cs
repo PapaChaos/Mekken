6224ba7c4efc714e78b6e0ba4de1e08cca65089b
Dan Henrik Wenger Wad <39692480+PapaChaos@users.noreply.github.com>
2019-01-23 01:26:38 +0100

    Explosions and Smoke!
    Added Smoke and Explosion particle. Also added custom script to change the explosion color based on blackbody temperature and decreasing the value over time.

---
+++ Assets/Particles/particle_Explosion.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particle_Explosion : MonoBehaviour
+{
+	public ParticleSystem ps;
+	public ParticleSystemRenderer psr;
+	public float blackbodyValue = 5000;
+	public Gradient grad = new Gradient();
+
+	public float EmissiveStrength = 5;
+
+	public float TimePassed;
+	public float TimeLeft;
+	public float TimeMax;
+
+	void Start()
+	{
+		var main = ps.main;
+		var colorLifeTime = ps.colorOverLifetime;
+
+		main.startColor = Mathf.CorrelatedColorTemperatureToRGB(blackbodyValue);
+		colorLifeTime.enabled = true;
+
+		grad.SetKeys(new GradientColorKey[] { new GradientColorKey(Mathf.CorrelatedColorTemperatureToRGB(5000), 0.0f), new GradientColorKey(Mathf.CorrelatedColorTemperatureToRGB(2500), 0.5f), new GradientColorKey(Mathf.CorrelatedColorTemperatureToRGB(500), 1f) }, new GradientAlphaKey[] { new GradientAlphaKey(1.0f, 0.0f), new GradientAlphaKey(0.0f, 1.0f) });
+		colorLifeTime.color = grad;
+	}
+	private void Update()
+	{
+		TimePassed += Time.deltaTime;
+		if (TimePassed == 0)
+			TimePassed = 0.01f;
+		TimeLeft = 1-(TimePassed/TimeMax);
+
+		blackbodyValue = (4500 * TimeLeft) + 500f;
+
+		psr.material.SetColor("_EmissionColor", Mathf.CorrelatedColorTemperatureToRGB(blackbodyValue) * EmissiveStrength); //"_EmisColor"
+
+	}
+}
