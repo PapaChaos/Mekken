e51677eda185a7f5a90e6a9f56cb7acaf2301620
joakimFF <joakimringqvist@gmail.com>
2019-01-27 20:30:58 +0100

    Added Initial Countdown Timer Display

---
+++ Assets/InitCountDown.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+public class InitCountDown : MonoBehaviour
+{
+    public int timeLeft = 60; //Seconds Overall
+    public Text countdown; //UI Text Object
+    public Text getReady;
+    public Text fight;
+    void Start()
+    {
+        StartCoroutine("LoseTime");
+        Time.timeScale = 1; //Just making sure that the timeScale is right
+    }
+    void Update()
+    {
+        if (timeLeft >= 0)
+        {
+            countdown.text = ("" + timeLeft); //Showing the Score on the Canvas
+            getReady.text = "GET READY!";
+            fight.text = "";
+        }
+        else
+        {
+            if (timeLeft >= -2)
+            {
+                countdown.text = ("");
+                getReady.text = ("");
+                fight.text = ("FIGHT!");
+            }
+            else
+            {
+                countdown.text = ("");
+                getReady.text = ("");
+                fight.text = ("");
+            }
+        }
+    }
+    //Simple Coroutine
+    IEnumerator LoseTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            timeLeft--;
+        }
+    }
+}
