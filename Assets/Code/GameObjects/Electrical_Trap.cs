11bda90db0cf4e757da0d334dd1510efb8c8093c
Spindsgud <46454301+Spindsgud@users.noreply.github.com>
2019-01-28 15:07:59 +0100

    git patch
    john is doing this

---
+++ Assets/Code/GameObjects/Electrical_Trap.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Electrical_Trap : MonoBehaviour
+{ 
+
+    public bool Power = true;
+    private float time = 0.0f; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        Debug.Log("collided with " + other.transform.name);
+
+        if (Power == false)
+        {
+            Debug.Log("Do nothing, power is off");
+
+        }
+        else
+        {
+
+            //Take dmg as player is inbound//
+            Debug.Log("AU");
+    
+        }
+    }
+}
