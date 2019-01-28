76759384c6c606aab871813a9e28cf2ebce949c5
Spindsgud <46454301+Spindsgud@users.noreply.github.com>
2019-01-28 19:20:15 +0100

    Update Electrical_Trap.cs

--- Assets/Code/GameObjects/Electrical_Trap.cs
+++ Assets/Code/GameObjects/Electrical_Trap.cs
@@ -6,18 +6,29 @@ public class Electrical_Trap : MonoBehaviour
 { 
 
     public bool Power = true;
-    private float time = 0.0f; 
+    private float time = 0.0f;   //keep track of time elapsed
 
     // Start is called before the first frame update
     void Start()
     {
-
+              
+        
+       
+        //get time now
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        //check if time elapsed is greater than time on
+
+        //if yes and power on, turn off power, reset time (get the time)
+
+        //if yes and power off, turn on power, reset time (get the time)
+
+        //otherwise, accumulate time
 
     }
 
