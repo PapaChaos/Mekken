7351e1f8b9e59d4988c18306681a4eef5a5df2de
Knutern <33419500+Knutern@users.noreply.github.com>
2019-01-29 15:06:06 +0000

    added play again

---
+++ Assets/PlayAgain/Main_Menu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Main_Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+       
+
+
+    
+
+      
+       
+
+
+
+
+}
