b439b9d3dc8ae945b4dacdc1c1440ad925e53eb3
Knutern <33419500+Knutern@users.noreply.github.com>
2019-01-29 15:32:35 +0000

    grrrrrrrr

---
+++ Assets/PlayAgain/LoadMainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadMainMenu : MonoBehaviour
+{
+    
+    public void LoadByIndex()
+    {
+
+        Debug.Log("HELLO LOAD");
+        SceneManager.LoadScene(1);
+    }
+
+}
