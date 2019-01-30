d747ad88a590e6d4399582158937372bfb713b6e
Knutern <33419500+Knutern@users.noreply.github.com>
2019-01-30 19:56:04 +0100

    Mine code

---
+++ Assets/Mine.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mine : MonoBehaviour
+{
+    public Renderer rend;
+    public GameObject explosionPrefab;
+    // Start is called before the first frame update
+    //Frasier & Katarina
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            Debug.Log("Start Explosion!!!");
+            rend.enabled = false;
+            explosionPrefab.SetActive(true);
+            Invoke("DestroyMine", 2);
+        }
+
+    }
+
+    void DestroyMine()
+    {
+
+        Destroy (gameObject);
+    }
+
+}
