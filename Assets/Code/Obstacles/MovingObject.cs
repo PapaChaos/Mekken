ba98d944f3dadda53cc5b6a9288347ac53b1a388
Dan Henrik Wenger Wad <39692480+PapaChaos@users.noreply.github.com>
2019-01-30 05:13:03 +0100

    ConstructionUI Update, MovingObject created.
    -The UI scaling for the ConstructionUI should now be fixed for all horizontal ratios. Not tried yet on vertical.
    
    -Made a complicated MovingObject (Obstacle) that designers should be able to easily use to make walls move at certain speeds. Might want to add an variable to pause between waypoints later. Use the Cube prefab to test.

---
+++ Assets/Code/Obstacles/MovingObject.cs
@@ -0,0 +1,106 @@
+﻿/*
+Created by Dan Wad. 30.01-19
+Think this should have everything that is needed. Might need some clean up and better commenting.
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour
+{
+
+	public List<Vector3> waypointLocation;
+	public List<Vector3> waypointRotation;
+
+	public int startingWaypoint = 0;
+	public int targetWaypoint = 1;
+	public int previousWaypoint;
+
+	public float movementSpeed = 1;
+	public bool reverseAtEnd;
+	public bool addPosition = true;
+	public bool moving = true;
+	float waypointDistance;
+	float alphaLerp;
+	float currentDistance;
+
+	void Awake()
+	{
+		gameObject.transform.position = waypointLocation[startingWaypoint];
+		gameObject.transform.eulerAngles = waypointRotation[startingWaypoint];
+		previousWaypoint = startingWaypoint;
+		targetWaypoint = startingWaypoint;
+
+	}
+
+    void Update()
+    {
+		if (moving) //in case we want in game activation and deactivation of object.
+			if (waypointLocation.Count > 1) //makes sure that we do this only if we got more than one waypoint.
+			{
+				float frameMovement = movementSpeed * Time.deltaTime;
+				transform.position = Vector3.MoveTowards(transform.position, waypointLocation[targetWaypoint], frameMovement);
+
+				
+				currentDistance = Vector3.Distance(gameObject.transform.position, waypointLocation[targetWaypoint]);
+				if (waypointDistance != 0) //we don't divide by zero.
+				{
+					alphaLerp = 1 - (currentDistance / waypointDistance);
+
+					transform.eulerAngles = Vector3.Lerp(waypointRotation[previousWaypoint], waypointRotation[targetWaypoint], alphaLerp);
+				}
+
+				if (currentDistance < 0.1f)
+				{
+					targetWaypoint = NextWaypoint();
+					waypointDistance = Vector3.Distance(waypointLocation[previousWaypoint], waypointLocation[targetWaypoint]);
+				}
+				
+
+
+			}
+        
+    }
+
+
+	int NextWaypoint()
+	{
+		int waypoint = targetWaypoint;
+		previousWaypoint = targetWaypoint;
+			switch (addPosition)
+			{
+				case true:
+					waypoint++;//just so I don't need another else statement.
+					if (waypoint >= waypointLocation.Count)
+					{
+						if (reverseAtEnd)
+						{
+							addPosition = false;
+							waypoint--;
+							waypoint--;
+						
+						}
+
+						else
+						{
+							waypoint = 0;
+						}
+
+					}
+				return waypoint;
+
+				case false:
+
+				waypoint--;
+
+					if (waypoint < 0)
+					{
+						addPosition = true;
+						waypoint = 1;
+					}
+				return waypoint;
+
+			}
+		return waypoint;
+	}
+}
---
+++ Assets/Editor/AddWaypoint.cs
@@ -0,0 +1,104 @@
+﻿/*
+Created by Dan Wad. 30.01-19
+ToDo: add a "add waypoint at number. Then move all waypoints above that number up by one. So if we have 5 waypoints and we want to add a waypoint at number 3. Then 3 becomes 4 and 4 becomes 5 (since we start at 0).
+
+
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+[CustomEditor(typeof(MovingObject))]
+public class AddWaypoint : Editor
+{
+	//private variables 
+	bool showWaypoints = true;
+	bool objectDebug = false;
+	int moveToWaypoint;
+
+
+	public override void OnInspectorGUI()
+	{
+		MovingObject mo = (MovingObject)target;
+
+		GUILayout.Label("Current Target Waypoint: " + mo.targetWaypoint);
+		if (objectDebug)
+			GUILayout.Label("Previous Waypoint: " + mo.previousWaypoint);
+		EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+		mo.movementSpeed = EditorGUILayout.FloatField("Movement Speed: ", mo.movementSpeed);
+		mo.startingWaypoint = EditorGUILayout.IntField("Starting Waypoint: ", mo.startingWaypoint);
+
+
+		//Just to make sure that the editer doesn't enter a bad value.
+		if (mo.startingWaypoint < 0 || mo.startingWaypoint >= mo.waypointLocation.Count && mo.waypointLocation.Count > 0)
+		{
+			//Just to make sure that the editer doesn't get confused by why the value is set to 0.
+			Debug.Log("There is no " + mo.startingWaypoint + " waypoint. Setting to 0.");
+
+			mo.startingWaypoint = 0;
+		}
+
+		mo.reverseAtEnd = EditorGUILayout.Toggle("Reverse at the end", mo.reverseAtEnd);
+
+		if (objectDebug)
+			mo.addPosition = EditorGUILayout.Toggle("adding to waypoint", mo.addPosition);
+
+		EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+		GUILayout.BeginHorizontal();
+		if (GUILayout.Button("Move to Waypoint: "))
+		{
+			mo.transform.position = mo.waypointLocation[moveToWaypoint];
+			mo.transform.eulerAngles = mo.waypointRotation[moveToWaypoint];
+		}
+		moveToWaypoint = EditorGUILayout.IntField(moveToWaypoint);
+
+		//Just to make sure that the editer doesn't enter a bad value.
+		if (moveToWaypoint >= mo.waypointLocation.Count || moveToWaypoint < 0)
+		{
+			//Just to make sure that the editer doesn't get confused by why the value is set to 0.
+			Debug.Log("There is no " + moveToWaypoint +" waypoint. Setting to 0.");
+			moveToWaypoint = 0;
+		}
+		GUILayout.EndHorizontal();
+		EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+
+		if (GUILayout.Button("Add Current Location and Rotation"))
+		{
+			mo.waypointLocation.Add(mo.transform.position);
+			mo.waypointRotation.Add(mo.transform.rotation.eulerAngles);
+		}
+
+
+		//Adding a fold out at the end for all the waypoints. This way EditorGUI doesn't look so massive.
+		showWaypoints = EditorGUILayout.Foldout(showWaypoints, "Waypoints: " + mo.waypointLocation.Count);
+		if (showWaypoints)
+			if (Selection.activeTransform)
+			{
+				for (int listcount = 0; listcount < mo.waypointLocation.Count; listcount++)
+				{
+					EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+					GUILayout.BeginHorizontal();
+					GUILayout.Label("     Waypoint: " + listcount);
+					if(GUILayout.Button("Remove")){
+						mo.waypointLocation.RemoveAt(listcount);
+						mo.waypointRotation.RemoveAt(listcount);
+					}
+					GUILayout.EndHorizontal();
+					mo.waypointLocation[listcount] = EditorGUILayout.Vector3Field("Waypoint Location:", mo.waypointLocation[listcount]);
+					mo.waypointRotation[listcount] = EditorGUILayout.Vector3Field("Waypoint Rotation:", mo.waypointRotation[listcount]);
+					
+					GUILayout.Space(5);
+				}
+				EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+
+
+			}
+		if (!Selection.activeTransform)
+		{
+			showWaypoints = false;
+		}
+	}
+
+}
