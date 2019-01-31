08d546a288165b27da0935ca1812920dd7630bea
johnklima <klima.de.sa@gmail.com>
2019-01-31 14:58:11 +0100

    2 player controllers

--- Assets/Code/PhysicsPlayer/playerControl.cs
+++ Assets/Code/PhysicsPlayer/playerControl.cs
@@ -18,14 +18,16 @@ public class playerControl : MonoBehaviour
     public bool firePrimary = false;
     public bool fireSecondary = false;
 
-    
+    public string playerNumberText = "1";  //by default, 1
+
+
     //is any control key pressed this frame (other than fire)?
     public bool controlerOn = false;
 
     // Start is called before the first frame update
     void Start()
     {
-    
+
     }
 
     // Update is called once per frame
@@ -44,66 +46,54 @@ public class playerControl : MonoBehaviour
 
         controlerOn = false;
 
-    
+
         //assuming player 1
         //TODO: handle 2 players
-        /* <JPK> not needed and indeed controlerOn should be false
-        if (Input.GetAxis("Horizontal Player 1") == 0)
-        {
-            right = false;
-            left = false;
 
-            controlerOn = true;
-        }
-        */
-       
-        if (Input.GetAxis("Horizontal Player 1") > 0.1)
+        // Horizontal
+        if (Input.GetAxis("Horizontal Player " + playerNumberText) > 0.1)
         {
             right = true;
-            //<JPK> not needed, all control booleans are cleared at the top
-            //left = false;
+
 
             controlerOn = true;
         }
-        if (Input.GetAxis("Horizontal Player 1") < -0.1)
+        if (Input.GetAxis("Horizontal Player " + playerNumberText) < -0.1)
         {
             left = true;
-            //<JPK> not needed, all control booleans are cleared at the top
-            //right = false;
+
 
             controlerOn = true;
         }
 
-        //<JPK> not needed, all control booleans are cleared at the top
-        /*
-        if (Input.GetAxis("Vertical Player 1") == 0)
-        {
-            forward = false;
-            backward = false;
 
-            controlerOn = true;
-        }
-        */
-
-        if (Input.GetAxis("Vertical Player 1") > 0.1f)
+        // Vertical
+        if (Input.GetAxis("Vertical Player " + playerNumberText) > 0.1f)
         {
             forward = true;
-            //backward = false;
+
 
             controlerOn = true;
         }
-        if (Input.GetAxis("Vertical Player 1") < -0.1f)
+        if (Input.GetAxis("Vertical Player " + playerNumberText) < -0.1f)
         {
             backward = true;
-            //forward = false;
+
 
             controlerOn = true;
         }
 
         //TODO: rapid fire or single shot?
-        if (Input.GetButton("Fire Player 1")) 
+        if (Input.GetButton("Fire Player " + playerNumberText))
         {
             firePrimary = true;
+        }
+
+
+        // Strafe
+        if (Input.GetButton("Strafe Player " + playerNumberText))  //combo key
+        {
+            strafe = true;
         }
 
         //TODO: control for power on missle launch
