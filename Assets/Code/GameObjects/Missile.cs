49d62038136ef479771d86ab7df01197a9a51fc4
johnklima <klima.de.sa@gmail.com>
2019-02-06 12:37:51 +0100

    missile updates and prefabs

--- Assets/Code/GameObjects/Missile.cs
+++ Assets/Code/GameObjects/Missile.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Missile : MonoBehaviour
 {
-        
+
 
     //these may be a function of the player, if they have control of cannon angle,
     //or it is an inherent property of the projectile/weapon system
@@ -12,13 +12,14 @@ public class Missile : MonoBehaviour
     public float powerFactor = 20.0f;
 
     public bool inAir = false;
+    public bool isInUse = false;
 
     //these we will eventual make private
     public Vector3 velocity;
     public Vector3 acceleration;
     public Vector3 finalForce;
 
-    public float  GRAVITY_CONSTANT = -9.8f;
+    public float GRAVITY_CONSTANT = -9.8f;
     public float missileAccel = 0.0f;
     public float damageValue = 1.0f;
 
@@ -35,10 +36,14 @@ public class Missile : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if(propulsionSystem)
+
+        inAir = false;
+        isInUse = false;
+
+        if (propulsionSystem)
             propulsionSystem.Stop();
 
-        if(hitSystem)
+        if (hitSystem)
             hitSystem.Stop();
 
         MissileHit = GetComponent<AudioSource>();
@@ -51,20 +56,25 @@ public class Missile : MonoBehaviour
     {
         if (inAir)
         {
-            handleMovementAir();            
+            handleMovementAir();
         }
 
         if (hitTimer > 0 && Time.time > hitTimer)
         {
             //reset timer
             hitTimer = -1;
+
             //send it to hell
             transform.position = new Vector3(0, -666, 0);
+
             //reshow
             transform.GetComponent<MeshRenderer>().enabled = true;
 
+            //let it be used again
+            isInUse = false;
+
         }
-        
+
     }
 
 
@@ -82,7 +92,7 @@ public class Missile : MonoBehaviour
 
         //point the missile
         transform.LookAt(transform.position + velocity);
-        
+
         //move the object
         transform.position += velocity * Time.deltaTime;
 
@@ -93,6 +103,7 @@ public class Missile : MonoBehaviour
 
         velocity = (direction + angle) * power * powerFactor;
         inAir = true;
+        isInUse = true;
 
         if (propulsionSystem)
             propulsionSystem.Play();
@@ -101,7 +112,7 @@ public class Missile : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-       
+
 
         //TODO: make sure missile only collides with the enemy, not with who launched it,
         //      except where it is a grenade or a mortar gone very wrong. in that case,
@@ -109,10 +120,10 @@ public class Missile : MonoBehaviour
         //      clears our collision box before it can explode
 
         //do damage for other player only
-        if ( other.transform.GetComponent<damage>() && other.transform != owner)
+        if (other.transform.GetComponent<damage>() && other.transform != owner)
         {
 
-            Debug.Log("collided with" + other.name);
+            Debug.Log("collided with player " + other.name);
             //make an explosion
 
             Debug.Log("BOOM!!!");
@@ -135,9 +146,35 @@ public class Missile : MonoBehaviour
             //apply force as an impulse, this needs to be fairly huge as it applies to velocity only once.
             other.transform.GetComponent<playerMotion>().applyImpulseForce(transform.forward * damageValue * 10000.0f);
 
-           
-            }
+
+
+        }
+        else if(other.transform != owner && other.tag!= "TargetCone" ) //it's not the owner and it's not the cone
+        {
+            //TODO: it hit something else, so explode? for grenades and mortars, we kinda want them to effect us too.
+            //      this is gonna need more thinking to exclude certain things in the collision set
+
+            //Debug.Log("collided with non-player " + other.name);
+
+            /*
+            Debug.Log("BOOM OTHER!!!");
+            inAir = false;
+
+            if (hitSystem)
+                hitSystem.Play();
+
+            if (propulsionSystem)
+                propulsionSystem.Stop();
+
+            MissileHit.Play();
+
+            hitTimer = Time.time + 2.0f;
+
+            transform.GetComponent<MeshRenderer>().enabled = false;
+            */
         }
 
     }
-   
+}
+
+
--- Assets/Code/PhysicsPlayer/FireProjectile.cs
+++ Assets/Code/PhysicsPlayer/FireProjectile.cs
@@ -6,14 +6,15 @@ public class FireProjectile : MonoBehaviour
 {
 
 
-    public float power = 1.0f;
-    public float cooldown = -1.0f;
+    public float power = 1.0f;    
     public float timeout = 1.0f;  
 
     public playerControl controller;
-    public Transform playerGeom;
-    public Missile missile;
+    public Transform playerGeom;    
+    public Transform missilePool;
 
+    private Missile missile;
+    private float cooldown = -1.0f;
 
     //TODO: decide how to set this based on mecha constructor, maybe just LEFT is secondary
     //      we could make a subclass, but just for one boolean??
@@ -39,6 +40,9 @@ public class FireProjectile : MonoBehaviour
         if(controller.firePrimary && cooldown < 0 && isPrimary)
         {
             cooldown = Time.time;
+
+            setMissile();
+
             fire();
         
         }
@@ -46,6 +50,9 @@ public class FireProjectile : MonoBehaviour
         if (controller.fireSecondary && cooldown < 0 && !isPrimary)
         {
             cooldown = Time.time;
+
+            setMissile();
+
             fire();
 
         }
@@ -54,14 +61,35 @@ public class FireProjectile : MonoBehaviour
 
     }
 
+    void setMissile()
+    {
+        foreach(Transform child in missilePool)
+        {
+            Missile tmisl = child.GetComponent<Missile>();
+            //get the first unused missile in the pool
+            if (tmisl.isInUse == false)
+            {
+                //got one so scoot
+                missile = tmisl;
+                tmisl.isInUse = true;
+                return;
+            }
+        }
+
+
+    }
+
     void fire()
     {
+
+        Debug.Log("WHY FIRE?");
+
         //move the missle into initial position
         missile.transform.position = playerGeom.position + playerGeom.forward + Vector3.up;
         missile.transform.forward = playerGeom.forward;
 
-        //own the missile
-        missile.owner = transform;
+        //own the missile, the controller PhysicsPlayer who fired it
+        missile.owner = controller.transform;
 
         //tell the missle to fire
         missile.fireMissile(playerGeom.forward, power);
