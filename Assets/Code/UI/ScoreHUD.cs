36ae23922b4c296a6c0854088409710d8b624d3b
Dan Henrik Wenger Wad <39692480+PapaChaos@users.noreply.github.com>
2019-02-11 01:59:27 +0100

    Quick scoreboard fix.
    -Made a quick script to check the gamemanager scores and set the scores accordingly.
    -Can look into a better way of doing score UI later if we want more players.

---
+++ Assets/Code/UI/ScoreHUD.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreHUD : MonoBehaviour
+{
+	public GameManager gm;
+	public Text player1Score;
+	public Text player2Score;
+	private void Awake()
+	{
+		gm = FindObjectOfType<GameManager>();
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		player1Score.text = gm.playerScore[0].ToString();
+		player2Score.text = gm.playerScore[1].ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		player1Score.text = gm.playerScore[0].ToString();
+		player2Score.text = gm.playerScore[1].ToString();
+	}
+}
